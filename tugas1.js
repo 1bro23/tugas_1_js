fc3a2f212ac07eb1a18430a0f278a5bcdc33ff4a
Ramdhawa <shinlia21@gmail.com>
2020-07-25 22:08:15 +0800

    add tugas 1 js

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var nama_depan="I Made Ramdhawa";
+let nama_belakang="Yudapatha";
+const umur=20;
+console.log("Nama Depan :",nama_depan)
+console.log("Nama Belakang : ",nama_belakang)
+console.log("Nama : ",nama_depan,nama_belakang)
+console.log("Umur : ",umur)
